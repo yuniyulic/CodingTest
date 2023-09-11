203e0cf19dda78d9d5a7585e165edb11a00d667f
yuni <77518429+yuniyulic@users.noreply.github.com>
2023-09-11 21:21:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int price) {
+        double answer = 0; // 지불해야 할 금액
+
+        if (price >= 500000 && price <= 1000000) {
+            answer = price - (price * 0.2); // 50만원 이상 구매 시 20% 할인
+        } else if (price >= 300000) {
+            answer = price - (price * 0.1); // 30만원 이상 구매 시 10% 할인
+        } else if (price >= 100000) {
+            answer = price - (price * 0.05); // 10만원 이상 구매 시 5% 할인
+        } else {
+            answer = price; // 10만원 미만 구매 시 정가
+        }
+
+        return (int)answer; // 소수점 이하 버리기
+        }
+}
