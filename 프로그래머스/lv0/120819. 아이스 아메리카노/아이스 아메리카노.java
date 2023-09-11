c428a8bcdda2bbb9a82f45233db7705229cc43df
yuni <77518429+yuniyulic@users.noreply.github.com>
2023-09-11 22:15:40 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -2,13 +2,8 @@ class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
         
-        if (money < 5500) {
-            answer[0] = 0;
-            answer[1] = money;
-        } else {
-            answer[0] = money / 5500; // 아메리카노 개수
-            answer[1] = money % 5500; // 남는 돈
-        }
+        answer[0] = money / 5500; // 아메리카노 개수
+        answer[1] = money % 5500; // 남는 돈
         
         return answer;
     }
