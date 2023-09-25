096a511ae49a8dc07fbba17c8b8519c22ec25810
yuni <77518429+yuniyulic@users.noreply.github.com>
2023-09-25 23:42:45 +0900

    [level 0] Title: 가위 바위 보, Time: 2.26 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] strArr = rsp.split("");
+        
+        for (int i=0; i<rsp.length(); i++) {
+            if (strArr[i].equals("0")) {answer += "5";}
+            else if (strArr[i].equals("2")) {answer += "0";}
+            else if (strArr[i].equals("5")) {answer += "2";}
+        }
+        return answer;
+    }
+}
