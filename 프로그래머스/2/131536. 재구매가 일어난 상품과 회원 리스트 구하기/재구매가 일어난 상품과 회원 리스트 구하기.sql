e6c66d64b59a2a47195253bd5582fac914f1e976
yuni <77518429+yuniyulic@users.noreply.github.com>
2024-12-08 20:50:29 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT
+    user_id,
+    product_id
+from
+    ONLINE_SALE
+group by
+    user_id,
+    product_id
+having
+    count(*) > 1
+order by
+    user_id, 
+    product_id desc
+;
+
+
+
